f43f9fb3523030d7d1383763f17986a014ecafba
Dilip Birajadar <dilip.birajadar@gmail.com>
2020-06-14 02:27:44 +0530

    first commit for the Room database.

---
+++ app/src/main/java/com/dilip/room/WordRepository.kt
@@ -0,0 +1,18 @@
+package com.dilip.room
+
+import androidx.lifecycle.LiveData
+
+/**
+ *Declares the DAO as a private property in the constructor. Pass in the DAO
+ * instead of the whole database, because you only need access to the DAO
+ */
+class WordRepository(private val wordDao: WordDao){
+    // Room executes all queries on a separate thread.
+    // Observed LiveData will notify the observer when the data has changed.
+    val allWord : LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+    suspend fun insert(word: Word){
+        wordDao.insert(word)
+    }
+
+}
